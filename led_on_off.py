023ee9605b6077e9bdcccb6bfeec03fbad5e21a4
Manooj Kumar <manoojkumar@aroliant.com>
2017-08-28 04:49:55 +0000

    Sample program updated

--- led_on_off.py
+++ led_on_off.py
@@ -12,12 +12,25 @@ led =  port.PA12
 gpio.init()
 gpio.setcfg(led, gpio.OUTPUT)
 
-IoT = BIoT("localhost:",5000)
+
+# LED Test
+gpio.output(led, 0)
+sleep(1)
+gpio.output(led,1)
+sleep(1)
+gpio.output(led, 0)
+
+
+DEVICE_ID =  ""
+DEVICE_TOKEN = "12345"
+
+
+IoT = BIoT("192.168.100.10",5000,params = { 'token' : DEVICE_TOKEN })
 
 IoT.wait(2)
 
 def light_state_change(status):
-	if status == "0":
+	if status == 0:
 		gpio.output(led, 0)
 		print("LIGHT OFF")
 	else:
@@ -26,7 +39,7 @@ def light_state_change(status):
 	print(status )
 
 
-IoT.on_param_change('status','1',light_state_change)
+IoT.on_param_change('status',1,light_state_change)
 
 IoT.wait()
 
