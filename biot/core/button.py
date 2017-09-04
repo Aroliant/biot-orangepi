19cfb73ea15ce1c91494d99f5786ce203190fe12
Manooj Kumar <manoojkumar@aroliant.com>
2017-09-04 18:02:20 +0530

    Library outlined

---
+++ biot/core/button.py
@@ -0,0 +1,10 @@
+class Button():
+	"""Default Device with ON / OFF Functions"""
+	deviceID = None
+	def __init__(self, deviceID):
+		if deviceID is None:
+			print("Provide a Device ID")
+			return
+		self.deviceID = deviceID
+
+	
---
+++ sample.py
@@ -0,0 +1,7 @@
+from biot.core.led import LED
+
+myLed = LED(1,{},{})
+
+
+print(myLed.__dict__)
+print(dir(myLed))
